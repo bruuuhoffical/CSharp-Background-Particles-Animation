cd648a0be42e9d3d1b46b7d24fdace7142ca157c
bruuuhoffical <bruuuhofficial@gmail.com>
2025-07-12 00:49:00 +0100

    Add project files.

---
+++ Particles/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Particles
+{
+    public partial class Form1 : Form
+    {
+        private ParticleSystem particleSystem;
+        public Form1()
+        {
+            InitializeComponent();
+            particleSystem = new ParticleSystem();
+        }
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+            e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+            particleSystem.DrawParticles(e.Graphics);
+        }
+        private void particles_Tick(object sender, EventArgs e)
+        {
+            particleSystem.UpdateParticles();
+            Invalidate();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
---
+++ Particles/Particles/Readme.txt
@@ -0,0 +1,32 @@
+﻿==================== MADE BY GEFREZIM (BRUUUH CHEATS) ======================
+
+Warining if you this project or code is seen in another server without permision or credit you wil be banned form the server permernenlty !!!
+
+============================== BRUUUH ====================================
+
+
+If you want to change your project / panel to another shape 
+
+
+Steps
+
+
+1.Drag the file out of the Particles Folder
+2.Rename it from (name.txt) to (name.cs)
+
+Thats all enjoy
+
+
+
+How to integrate it to your main form
+
+1.Make a Timer
+2. Enabled it
+3. Interval - 3
+4. Double Click and add this >>>
+    -- particleSystem.UpdateParticles();
+    -- Invalidate();
+
+5. Make sure you add these too for more info or paid file dm me
+
+Gefrezim || bruuuhofficial01 OR https://discord.com/users/1200223126689161247
